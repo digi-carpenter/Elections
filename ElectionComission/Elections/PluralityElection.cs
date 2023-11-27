b1c0bf6defce08f577f1f673e25a76cc4d49dbff
Sean O'Leary <sean.oleary@zip.co>
2023-11-27 11:08:13 -0600

    Adding DSL and execution for PluralityElection

--- ElectionComission/Elections/PluralityElection.cs
+++ ElectionComission/Elections/PluralityElection.cs
@@ -2,10 +2,14 @@ using ElectionComission.Interfaces;
 
 namespace ElectionComission.Elections;
 
-public class PluralityElection : IElection<ISingleVoteBallot>
+public sealed class PluralityElection : IElection<ISingleVoteBallot>
 {
     public ICandidate Run(IReadOnlyList<ISingleVoteBallot> ballots, IReadOnlyList<ICandidate> candidates)
     {
-        throw new NotImplementedException();
+        // NOTE: My choice here is to go with a DSL based approach based on LINQ since the calculation is straight forward. The benifit is that
+        // it's succinct and easy to read in the context of the election, but it's also easy to test outside of the election class.
+        return ballots
+            .TallyValidVotes(candidates)
+            .SelectPluralityWinner();
     }
 }
--- ElectionComission/Tallies.cs
+++ ElectionComission/Tallies.cs
@@ -4,5 +4,47 @@ namespace ElectionComission;
 
 public static class Tallies
 {
+    /// <summary>
+    /// Tallies the provided ballots of valid candidates by candidate.
+    /// </summary>
+    /// <param name="ballots">The ballots.</param>
+    /// <returns><see cref="ITally"/></returns>
+    public static IEnumerable<ITally> TallyValidVotes(this IReadOnlyList<ISingleVoteBallot> ballots, IEnumerable<ICandidate> validCandidates)
+    {
+        var tallies = ballots
+                        .GroupBy(ballot => ballot.Vote.Candidate)
+                        .Select(
+                            ballotGroup => new CandidateTally(ballotGroup.Key, ballotGroup.Count()))
+                        .ForSpecificCandidates(validCandidates);
+
+        return tallies;
+    }
+    
+    /// <summary>
+    /// Selects the candidate with the majority of votes, albeit not necessarily absolute majority.
+    /// </summary>
+    /// <param name="tallies">The tally.</param>
+    /// <returns><see cref="ICandidate"/></returns>
+    public static ICandidate SelectPluralityWinner(this IEnumerable<ITally> tallies)
+    {
+        return tallies
+                .OrderByDescending(candidate => candidate.Votes)
+                .First()
+                .Candidate;
+    }
+
+	/// <summary>
+    /// Removes invalid candidates from the collection of tallies.
+    /// </summary>
+    /// <param name="tallies">The tallies.</param>
+    /// <param name="validCandidates">The valid candidates.</param>
+    private static IEnumerable<ITally> ForSpecificCandidates(this IEnumerable<ITally> tallies, IEnumerable<ICandidate> validCandidates)
+    {
+        return tallies.Join(validCandidates,
+                            tally => tally.Candidate,
+                            candidate => candidate,
+                            (tally, candidate) => tally);
+    }
+
     private record CandidateTally(ICandidate Candidate, int Votes) : ITally;
 }
